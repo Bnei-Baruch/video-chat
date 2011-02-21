4cb6435417ff84efa63ea83c99717063a21523df
gshilin <gshilin@gmail.com>
2011-02-21 12:05:38 +0200

    new design

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110213091318) do
+ActiveRecord::Schema.define(:version => 20110216112207) do
+
+  create_table "chats", :force => true do |t|
+    t.integer  "room"
+    t.text     "message"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "open_tok_sessions", :force => true do |t|
     t.string   "session_title"
